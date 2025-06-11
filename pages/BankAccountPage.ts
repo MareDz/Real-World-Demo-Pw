cd2cbe52f782fd92f64a14e44782d8c5c7747601
Marko Dz <marko.dzelajlija@htecgroup.com>
2025-06-11 12:49:00 +0200

    new tcs added

--- pages/BankAccountPage.ts
+++ pages/BankAccountPage.ts
@@ -1,13 +1,52 @@
-import { Page, Locator } from '@playwright/test'
+import { Page, Locator, expect } from '@playwright/test'
 import { BasePage } from './BasePage'
 import { UserData } from '../state/UserModel'
 
 export class BankAccountPage extends BasePage {
   readonly page: Page
+  readonly btn_createNewBankAccount: Locator
+  readonly btn_deleteBankAccount: Locator
+  readonly lbl_createBankAccount: Locator
+  readonly lbl_bankAccounts: Locator
+  readonly li_bankAccounts: Locator
 
   constructor(page: Page, ctx: UserData) {
     super(page, ctx)
     this.page = page
     this.ctx = ctx
+    this.btn_createNewBankAccount = page.locator("[data-test='bankaccount-new']")
+    this.btn_deleteBankAccount = page.locator("//button[@data-test='bankaccount-delete']")
+    this.lbl_createBankAccount = page.locator("//h2[text()='Create Bank Account']") // TODO: ID
+    this.lbl_bankAccounts = page.locator("//h2[text()='Create Bank Accounts']")
+    this.li_bankAccounts = page.locator("//ul[@data-test='bankaccount-list']")
+  }
+
+/**
+ * Verifies that the most recently added bank account is correctly displayed in the list.
+ * 
+ * Since new bank accounts are appended to the end of the list, this method targets the last
+ * element and checks if its text matches the expected bank name from the test context.
+ * 
+ * Useful in cases where we want to confirm that a newly created account has been successfully added
+ * without relying on the full list structure or order of existing accounts.
+ */
+  async verifyBankAccountDisplayed() {
+    console.log("BankAccountPage - verifyBankAccountDisplayed()")
+
+    const lbl_recendAddedBankAccount = this.li_bankAccounts.locator("//li//p").last()
+    await this.assertInnerText(lbl_recendAddedBankAccount, String(this.ctx.bank.bankName))
+  }
+
+  /**
+   * Clicks on the "Create New Bank Account" button.
+   * Waits for the bank account creation page to load.
+   * Verifies that the page contains the expected label or heading ("Create Bank Account").
+   */
+  // TODO: Explain better
+  async clickCreateNewBankAccount() {
+    console.log('BankAccountPage - clickCreateNewBankAccount()')
+    await this.btn_createNewBankAccount.click()
+    await this.assertTitleAndUrl('Cypress Real World App', 'bankaccounts/new')
+    await this.assertInnerText(this.lbl_createBankAccount, 'Create Bank Account')
   }
 }
--- tests/BankAccount.spec.ts
+++ tests/BankAccount.spec.ts
@@ -1,5 +1,5 @@
 import { Base } from '../pages/Base'
-import { GET_getNewUserData, POST_registerUser } from '../utils/apiHelpers'
+import { GET_getNewUserData, POST_createBankAccount, POST_loginUser, POST_registerUser } from '../utils/apiHelpers'
 import { test } from '../utils/fixturesModel'
 import { getAccountNumber, getBankName, getRoutingNumber } from '../utils/fnHelpers'
 
@@ -9,30 +9,56 @@ test.beforeAll(async () => {
   Base.initializeEnvironmentCRWA()
 })
 
-test.beforeEach(async ({ ctx, request, loginPage, onboardingPage }) => {
-  //   await GET_getNewUserData(ctx)
-  //   await POST_registerUser(request, ctx)
-  //   await loginPage.launchRWA()
-  //   await loginPage.login()
-  //   await onboardingPage.completeOnboardingProcessWithRandomBankData()
+test.beforeEach(async ({ loginPage }) => {
+  await loginPage.launchRWA()
 })
 
 test.afterEach(async ({ page }) => {
   await page.close()
 })
 
-test.only('Bank Account - Created Bank Account During Onboarding Process Displayed in Bank Details', async ({ ctx, registrationPage, loginPage, onboardingPage, sideNavPage, bankAccountPage }) => {
+test('Bank Account - Created Bank Account During Onboarding Process Displayed in Bank Details', async ({ ctx, registrationPage, loginPage, onboardingPage, sideNavPage, bankAccountPage }) => {
   await GET_getNewUserData(ctx)
-  await loginPage.launchRWA()
   await loginPage.goToRegistrationPage()
   await registrationPage.completeRegistrationForm()
   await loginPage.login()
+  await onboardingPage.verifyGetStartedIsDisplayed()
+  await onboardingPage.clickNextGetStartedAndVerifyCreateBankAccountDisplayed()
   await onboardingPage.completeBankAccountForm(getBankName(), getRoutingNumber(), getAccountNumber())
   await onboardingPage.verifyFinishedScreenDisplayed()
   await onboardingPage.clickDoneAndVerifyUserCredentials()
   await sideNavPage.goToBankAccounts()
+  await bankAccountPage.verifyBankAccountDisplayed()
 })
 
-test.skip('Bank Account - Empty Required Fields Validation', async ({ loginPage, sideNavPage }) => {
-  await sideNavPage.goToBankAccounts()
+test('Bank Account - Created Initial Bank Account (from API) Displayed in Bank Details', async ({ ctx, request, loginPage, onboardingPage, sideNavPage, bankAccountPage }) => {
+    await GET_getNewUserData(ctx)
+    await POST_registerUser(request, ctx)
+    await POST_loginUser(request, ctx)
+    await POST_createBankAccount(request, ctx, getBankName(), getRoutingNumber(), getAccountNumber())
+    await loginPage.login()
+    await sideNavPage.goToBankAccounts()
+    await bankAccountPage.verifyBankAccountDisplayed()
+})
+
+test('Bank Account - Empty Required Fields Validation', async ({ ctx, request, loginPage, onboardingPage, sideNavPage, bankAccountPage }) => {
+    await GET_getNewUserData(ctx)
+    await POST_registerUser(request, ctx)
+    await POST_loginUser(request, ctx)
+    await POST_createBankAccount(request, ctx, getBankName(), getRoutingNumber(), getAccountNumber())
+    await loginPage.login()
+    await sideNavPage.goToBankAccounts()
+    await bankAccountPage.clickCreateNewBankAccount()
+    await bankAccountPage.verifyBankAccountEmptyFieldErrorHandling()
+})
+
+test('Bank Account - Invalid Data Fields Validation', async ({ ctx, request, loginPage, onboardingPage, sideNavPage, bankAccountPage }) => {
+    await GET_getNewUserData(ctx)
+    await POST_registerUser(request, ctx)
+    await POST_loginUser(request, ctx)
+    await POST_createBankAccount(request, ctx, getBankName(), getRoutingNumber(), getAccountNumber())
+    await loginPage.login()
+    await sideNavPage.goToBankAccounts()
+    await bankAccountPage.clickCreateNewBankAccount()
+    await bankAccountPage.verifyBankAccountInvalidInputErrors()
 })
